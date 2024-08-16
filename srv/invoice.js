d994e7e2e6d91e8fd4bc3dd85c78fbcc95829fd7
kothahemanth <hemanthkotha17@gmail.com>
2024-08-16 14:05:51 +0530

    Update for BillingInfo and BillingItem

--- srv/invoice.js
+++ srv/invoice.js
@@ -4,6 +4,54 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = cds.service.impl(async function () {
     const billingapi = await cds.connect.to('API_BILLING_DOCUMENT_SRV');
 
+
+    this.on('READ', 'BillingInfo', async req => {
+        
+        // Adjust the query to correctly expand the CompanyCode and include all necessary columns
+        req.query.SELECT.columns = [
+            { ref: ['BillingDocument'] },
+            { ref: ['SDDocumentCategory'] },
+            { ref: ['SalesOrganization'] },
+            { ref: ['BillingDocumentDate'] },
+            { ref: ['TotalNetAmount'] },
+            { ref: ['FiscalYear'] },
+            {
+                ref: ['CompanyCode'],
+            }
+        ];
+
+        try {
+            let res = await billingapi.run(req.query);
+
+            return res;
+        } catch (error) {
+            console.error('Error during request to remote service:', error);
+            req.error(502, 'Error during request to remote service');
+        }
+    });
+
+    this.on('READ', 'BillingItem', async req => {
+        
+        // Adjust the query to correctly expand the CompanyCode and include all necessary columns
+        req.query.SELECT.columns = [
+            { ref: ['BillingDocumentItem'] },
+            { ref: ['BillingDocumentItemText'] },
+            { ref: ['BaseUnit'] },
+            { ref: ['BillingQuantityUnit'] },
+            { ref: ['Plant'] },
+            { ref: ['StorageLocation'] },
+            { ref: ['BillingDocument'] },
+        ];
+
+        try {
+            let res = await billingapi.run(req.query);
+
+            return res;
+        } catch (error) {
+            console.error('Error during request to remote service:', error);
+            req.error(502, 'Error during request to remote service');
+        }
+    });
     // Fetch and upsert data for Billing and BillingItems entities
     this.before('READ', ['Billing', 'BillingItems'], async req => {
         const { Billing, BillingItems } = this.entities;
