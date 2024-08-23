77cdb9b7d9e9d5f4d3a58363f57c061b1b401c45
kothahemanth <hemanthkotha17@gmail.com>
2024-08-23 16:04:08 +0530

    Billing Docs in Batches

--- srv/invoice.js
+++ srv/invoice.js
@@ -1,4 +1,3 @@
-
 const cds = require("@sap/cds");
 const { v4: uuidv4 } = require("uuid");
 
@@ -10,7 +9,7 @@ module.exports = cds.service.impl(async function () {
     try {
       const { Billing, BillingItems } = this.entities;
 
-      // Fetch existing records
+      // Fetch existing records (move this before batch processing starts)
       const existingBillingDocs = await cds.run(
         SELECT.from(Billing).columns(["BillingDocument"])
       );
@@ -30,32 +29,62 @@ module.exports = cds.service.impl(async function () {
         )
       );
 
-      // Fetch new Billing documents
-      let billingDocuments = await billingapi.run(
-        SELECT.from("API_BILLING_DOCUMENT_SRV.A_BillingDocument").columns([
-          "BillingDocument",
-          "SDDocumentCategory",
-          "SalesOrganization",
-          "BillingDocumentDate",
-          "TotalNetAmount",
-          "FiscalYear",
-          "CompanyCode",
-          "LastChangeDateTime",
-        ])
+      // Fetch the last synchronization date
+      const lastsyncdate1 = await cds.run(
+        SELECT.one.from(Billing)
+          .columns("LastChangeDateTime")
+          .orderBy("LastChangeDateTime desc")
       );
 
-      // Filter out existing Billing documents
-      const uniqueBillingDocuments = billingDocuments.filter(
-        (doc) => !existingBillingDocsMap.has(doc.BillingDocument)
-      );
-      const billingDocsToUpsert = uniqueBillingDocuments.map((doc) => ({
-        ID: uuidv4(),
-        ...doc,
-      }));
+      let billlastsyncdatetime;
+      if (lastsyncdate1) {
+        billlastsyncdatetime = lastsyncdate1.LastChangeDateTime;
+      }
 
-      // UPSERT Billing documents
-      if (billingDocsToUpsert.length > 0) {
-        await cds.run(UPSERT.into(Billing).entries(billingDocsToUpsert));
+      // Count and fetch billing documents based on last sync date
+      let countbilldocs;
+      let billdocqry = SELECT.from("API_BILLING_DOCUMENT_SRV.A_BillingDocument").columns([
+        "BillingDocument",
+        "SDDocumentCategory",
+        "SalesOrganization",
+        "BillingDocumentDate",
+        "TotalNetAmount",
+        "FiscalYear",
+        "CompanyCode",
+        "LastChangeDateTime",
+      ]);
+
+      if (billlastsyncdatetime) {
+        countbilldocs = await billingapi.send({
+          method: "GET",
+          path: `A_BillingDocument/$count?$filter=LastChangeDateTime gt datetimeoffset'${billlastsyncdatetime}'`,
+        });
+        billdocqry = billdocqry.where({ LastChangeDateTime: { gt: billlastsyncdatetime } });
+      } else {
+        countbilldocs = await billingapi.send({ method: "GET", path: "A_BillingDocument/$count" });
+      }
+      console.log(countbilldocs)
+
+      // Process in batches of 5000
+      let batchSize = 50;
+      for (let i = 0; i < countbilldocs; i += batchSize) {
+        let billingDocuments = await billingapi.run(billdocqry.limit(batchSize, i));
+        
+        console.log(`Processing Batch ${i + 1} of ${countbilldocs} records`);
+
+        // Filter out existing Billing documents
+        const uniqueBillingDocuments = billingDocuments.filter(
+          (doc) => !existingBillingDocsMap.has(doc.BillingDocument)
+        );
+        const billingDocsToUpsert = uniqueBillingDocuments.map((doc) => ({
+          ID: uuidv4(),
+          ...doc,
+        }));
+
+        // UPSERT Billing documents
+        if (billingDocsToUpsert.length > 0) {
+          await cds.run(UPSERT.into(Billing).entries(billingDocsToUpsert));
+        }
       }
 
       // Fetch new Billing items
